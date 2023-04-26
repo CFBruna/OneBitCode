f14d5c855c9a9e91c0ff734c8a9048849e88cc79
CFBruna <brunaads.ti@gmail.com>
2023-04-26 17:46:37 -0300

    Exercícios com if else

--- JavaScript/EXERCICIOS/CalculoDeDano/index.js
+++ JavaScript/EXERCICIOS/CalculoDeDano/index.js
@@ -1,7 +1,40 @@
-const nameCharacter1 = prompt ("Insira o nome do seu personagem:")
-const attackPower = prompt("Insira o poder de Ataque do seu personagem:")
-const nameCharacter2 = prompt("Insira o nome do segundo personagem:")
-const health = prompt("Insira quanto de vida o segundo personagem tem:")
-const defense = prompt("Insira quanto de defesa o segundo personagem tem:")
-const shield = prompt("Insira o quanto de escudo seu personagem tem:")
+const attackCharacter = prompt("Insira o nome do personagem atacante:");
+const attackPower = prompt("Insira o poder de ataque do personagem atacante:");
 
+const defenseCharacter = prompt("Insira o nome do personagem defensivo:");
+let health = prompt(
+  "Insira quantos pontos de vida o personagem defensivo tem:"
+);
+const defensePower = prompt("Qual poder de defesa o personagem defensivo tem:");
+const shield = prompt("O personagem defensivo possui escudo? (Sim/Não)");
+
+let damage = 0;
+
+if (attackPower > defensePower && shield === "Não") {
+  damage = attackPower - defensePower;
+} else if (attackPower > defensePower && shield === "Sim") {
+  damage = (attackPower - defensePower) / 2;
+}
+
+health -= damage;
+
+alert(
+  attackCharacter +
+    " causou " +
+    damage +
+    " pontos de dano em " +
+    defenseCharacter
+);
+alert(
+  attackCharacter +
+    "\nPoder de ataque: " +
+    attackPower +
+    "\n\n" +
+    defenseCharacter +
+    "\nPontos de vida: " +
+    health +
+    "\nPoder de defesa: " +
+    defensePower +
+    "\nPossui escudo: " +
+    shield
+);
